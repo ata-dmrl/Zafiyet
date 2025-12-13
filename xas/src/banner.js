7fe2370e69e220502046d011798a57f427b9791f
ata-dmrl <atakand805@gmail.com>
2025-12-13 23:06:09 +0300

    13.12.2025

---
+++ xas/src/banner.js
@@ -0,0 +1,15 @@
+function banner() {
+  console.log(`
+██   ██  █████  ███████ 
+ ██ ██  ██   ██ ██      
+  ███   ███████   ████   
+ ██ ██  ██   ██      ██      
+██   ██ ██   ██ ███████ 
+----------------------------------------
+        X A S   S E C U R I T Y
+   Active Recon & Analysis Framework
+----------------------------------------
+`);
+}
+
+module.exports = { banner };
